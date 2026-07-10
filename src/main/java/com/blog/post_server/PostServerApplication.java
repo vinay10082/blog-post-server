0531165a4810c8bd6075d5301fd1bd20a8a36b2e
vinay10082 <vinay06102001@gmail.com>
2026-07-11 04:40:26 +0530

    initiate the java backend code

---
+++ src/main/java/com/blog/post_server/PostServerApplication.java
@@ -0,0 +1,13 @@
+package com.blog.post_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/post_server/PostServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.post_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
