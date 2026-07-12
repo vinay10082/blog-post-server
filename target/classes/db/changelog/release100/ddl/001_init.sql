d8b63c687efde60d851acf548e97dc061ffe781d
vinay10082 <vinay06102001@gmail.com>
2026-07-13 01:02:18 +0530

    Cleanup and update gitignore

--- target/classes/db/changelog/release100/ddl/001_init.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset vinaykc:1
-CREATE SCHEMA IF NOT EXISTS core;
-CREATE TABLE IF NOT EXISTS sample_post (
-    id BIGSERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT
-);
