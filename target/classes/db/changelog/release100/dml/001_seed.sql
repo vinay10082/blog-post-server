aaf2cb978a3108af1766597c838152048ecc0531
vinay10082 <vinay06102001@gmail.com>
2026-07-12 16:36:07 +0530

    feat: parameterize DB_SCHEMA

--- target/classes/db/changelog/release100/dml/001_seed.sql
+++ target/classes/db/changelog/release100/dml/001_seed.sql
@@ -1,4 +1,9 @@
 -- liquibase formatted sql
 
 -- changeset vinaykc:2
-INSERT INTO sample_post (title, content) VALUES ('First Post', 'This is the first post created by liquibase.');
+INSERT INTO users (username, password, email)
+VALUES ('seed', '__seed__', 'seed@example.com')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO sample_post (title, content, author_id)
+VALUES ('First Post', 'This is the first post created by liquibase.', (SELECT id FROM users WHERE username = 'seed'));
